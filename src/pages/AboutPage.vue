004c80796fc395d8ffefe91f0e7ccd70739d800c
andreadatri <andrea.datri80@gmail.com>
2024-12-15 23:00:57 +0100

    add Chi siamo

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ciop">
-    <h1>About Page</h1>
+    <h1>Chi Siamo</h1>
     <p>Questa è la pagina About del nostro sito. Siamo lieti che tu sia qui</p>
   </div>
 </template>
