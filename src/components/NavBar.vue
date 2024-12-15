e9aff77d85655e734a7a3044bb50316f855ef57e
Andrea D'Atri <andrea.datri80@gmail.com>
2024-12-15 15:15:22 +0000

    Add

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <ul>
-      <li><router-link to="/">Pagina iniziale</router-link></li>
+      <li><router-link to="/">Home</router-link></li>
       <li><router-link to="/about">About</router-link></li>
       <li><router-link to="/contact">Contact</router-link></li>
     </ul>
